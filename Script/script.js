bbecf13008caad5fd8693667d24b26712af78caf
mage <mag@localhost.localdomain>
2019-09-11 15:52:49 +0530

    added show file and bug fixes

--- Script/script.js
+++ Script/script.js
@@ -1,5 +1,6 @@
 var dragobj = document.getElementsByName("file")
 window.addEventListener("change",function (){
   console.log("OnChanged Called "+dragobj[0])
+  dragobj[1].style.opacity = '1'
   
 })
